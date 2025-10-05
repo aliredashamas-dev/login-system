dd151fd887f8b671061e998fbfb4ce81195d1c09
alireda-dev <allireza.shamas@gmail.com>
2025-10-05 10:33:36 +0300

    fixed conflict

--- login.py
+++ login.py
@@ -11,16 +11,39 @@ while attempts < 3:
     password = input("enter password:")
 
     if username == stored_username and password == stored_password:
+<<<<<<< HEAD
     	print("login_successful")
     	logged_in = True
     	break
     else:
     	attempts += 1
     	print("incorrect credentials. Attempts left:", 3 - attempts)
+=======
+        print("login_successful")
+        logged_in = True
+        break
+    else:
+        attempts += 1
+        print("incorrect credentials. Attempts left:", 3 - attempts)
+>>>>>>> bc75dca (2nd commit)
 
-    if not logged_in:
-        print("account locked")
+    
+if logged_in:
+    print("welcome to the system!")
+    N = int(input("enter a number: "))
+    if N <= 1: 
+        print("no prime numbers")
+    print(f"prime numbers between 2 and {N} are:1")
+    for num in range(2, N + 1):
+        is_prime = True
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(num, end=" ")
 
+<<<<<<< HEAD
 N = int(input("enter a number: "))
 print(f"prime numbers between 2 and {N} are:1")
 for num in range(2, N + 1):
@@ -31,5 +54,8 @@ for num in range(2, N + 1):
             break
     if is_prime:
         print(num, end=" ")
+=======
+else: print("account locked")
+>>>>>>> bc75dca (2nd commit)
 		
 		
